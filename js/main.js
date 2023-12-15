8ac2104a94fd0dc7f35cf92e0ff2b396fa1c8c4c
Thanh Ramen <thanhramen439@gmail.com>
2023-12-15 17:45:39 +0700

    Add axiosApi.js, postApi.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+import postApi from './api/postApi';
 
+async function main() {
+  const queryParams = {
+    _page: 1,
+    _limit: 5,
+  };
+  const all = await postApi.getAll(queryParams);
+  console.log('All data', all);
+  const id = await postApi.getById('lea319jollj7y1ql');
+  console.log('id data', id);
 
+  const id2 = await postApi.getById('lea319jollj7y1ql-test');
+  console.log('id2 data', id2);
+}
+
+main();
