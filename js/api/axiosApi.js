ecbe597275d166a82524c969a71c5269c6757d32
Sy Tuan Thanh Nguyen T <sy.tuan.thanh.t.nguyen@ericsson.com>
2023-12-16 07:53:04 +0700

    Add interceptor

--- js/api/axiosApi.js
+++ js/api/axiosApi.js
@@ -7,4 +7,41 @@ const axiosClient = axios.create({
   },
 });
 
+// interceptors: apply changes to all req and res
+// Add a request interceptor
+axiosClient.interceptors.request.use(
+  function (config) {
+    // Attach token to req if exists
+    const accessToken = localStorage.getItem('access_token');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+axiosClient.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data; // only get data from response
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    console.log('error response', error.response);
+    if (!error.response) throw new Error('Network error!!');
+
+    if (error.response.status === 404) console.log('File not found!');
+
+    return Promise.reject(error);
+  },
+);
+
 export default axiosClient;
--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 import postApi from './api/postApi';
 
 async function main() {
-  const queryParams = {
-    _page: 1,
-    _limit: 5,
-  };
-  const all = await postApi.getAll(queryParams);
-  console.log('All data', all);
-  const id = await postApi.getById('lea319jollj7y1ql');
-  console.log('id data', id);
-
-  const id2 = await postApi.getById('lea319jollj7y1ql-test');
-  console.log('id2 data', id2);
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    };
+    const all = await postApi.getAll(queryParams);
+    console.log('All data', all);
+    const id = await postApi.getById('lea319jollj7y1ql1111');
+    console.log('id data', id);
+  } catch (error) {
+    console.log('Error postApi', error);
+  }
 }
 
 main();
