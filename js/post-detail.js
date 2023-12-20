f6f1aba00c588137d951408d5abb6d05068c640c
Thanh Ramen <thanhramen439@gmail.com>
2023-12-20 22:34:11 +0700

    Add function show image on post detail with modal boostrap

--- js/post-detail.js
+++ js/post-detail.js
@@ -14,19 +14,65 @@ function updatePostDetail(data) {
   };
 
   // Update post detail
-  const postTitle = document.querySelector('.post-title-wrapper');
-  updateTextPost(postTitle, '#postDetailTitle', data.title);
-  updateTextPost(postTitle, '#postDetailAuthor', data.author);
+  updateTextPost(document, '#postDetailTitle', data.title);
+  updateTextPost(document, '#postDetailAuthor', data.author);
+  updateTextPost(document, '#postDetailDescription', data.description);
   updateTextPost(
-    postTitle,
+    document,
     '#postDetailTimeSpan',
     dayjs(data.updatedAt).format(' - YYYY/MM/DD HH:mm'),
   );
 }
 
+function handleClickImage() {
+  // get all image
+  const imageList = document.querySelectorAll('img[data-album="post-album"]');
+  const imgArr = [...imageList];
+
+  let index = 0;
+
+  // get modal 'lightbox'
+  const lightbox = document.getElementById('lightbox');
+  const lighboxModal = new bootstrap.Modal(lightbox);
+  const lightboxImg = lightbox.querySelector('img[data-id="lightboxImg"]');
+
+  document.addEventListener('click', (e) => {
+    const { target } = e;
+    if (target.dataset.album === 'post-album') {
+      index = imgArr.findIndex((img) => target === img);
+
+      // set image to lightbox and show
+      lightboxImg.src = target.src;
+      lighboxModal.show();
+    }
+  });
+
+  // handle pre/next button
+  const prevBtn = lightbox.querySelector('button[data-id="lightboxPrev"]');
+  const nextBtn = lightbox.querySelector('button[data-id="lightboxNext"]');
+
+  if (!prevBtn || !nextBtn) return;
+
+  prevBtn.addEventListener('click', () => {
+    // update index
+    index = (index + 2) % 3;
+    lightboxImg.src = imgArr[index].src;
+    lighboxModal.show();
+  });
+
+  nextBtn.addEventListener('click', () => {
+    // update index
+    index = (index + 1) % 3;
+    lightboxImg.src = imgArr[index].src;
+    lighboxModal.show();
+  });
+}
+
 // main
 (async () => {
   try {
+    handleClickImage();
+
     //get params
     const url = new URL(window.location);
     const id = url.searchParams.get('id');
